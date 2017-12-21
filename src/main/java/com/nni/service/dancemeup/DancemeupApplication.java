032487ec32c06997b0a04f9b1d5764983ad5022c
garmonma <garmonmarcus@gmail.com>
2017-12-21 01:21:02 -0700

    Initial commit

---
+++ src/main/java/com/nni/service/dancemeup/DancemeupApplication.java
@@ -0,0 +1,12 @@
+package com.nni.service.dancemeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DancemeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DancemeupApplication.class, args);
+	}
+}
